f3470ef784a1243c06b6a12c032f41c7c1118e6c
NguyenDuyPhuongDeveloper <phuongnd0239@gmail.com>
2024-05-09 01:57:25 +0700

    create verification api

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -2,8 +2,18 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const UserModel = require('../models/userModel');
 const asyncHandle = require('express-async-handler');
+const nodemailer = require("nodemailer");
+require('dotenv').config();
 
 
+const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD,
+    },
+});
 // Create JSON Web Token
 const getJsonWebToken = (email, id) =>
 {
@@ -14,6 +24,7 @@ const getJsonWebToken = (email, id) =>
 
     return token;
 };
+
 
 // Register a new user
 const register = asyncHandle(async (req, res) =>
@@ -58,6 +69,45 @@ const register = asyncHandle(async (req, res) =>
     }
 
 });
+const handleSendMail = async (val, email) =>
+{
+
+    try
+    {
+        await transporter.sendMail({
+            from: `Support Yami Booking Application <${ process.env.EMAIL }>`, // sender address
+            to: email, // list of receivers
+            subject: "Verification email code", // Subject line
+            text: "Your code to verification email!", // plain text body
+            html: `<h1>${ verificationCode }</h1>`// html body
+        });
+        return 'Send email successfully!'
+
+    } catch (error)
+    {
+        return error
+    }
+};
+const verification = asyncHandle(async (req, res) =>
+{
+    const { email } = req.body;
+    const verificationCode = Math.round(1000 + Math.random() * 9000);
+    try
+    {
+        await handleSendMail(verificationCode, email);
+        res.status(200).json({
+            message: 'Send verification code successfully!  ',
+            data: {
+                verificationCode
+            }
+        });
+    } catch (error)
+    {
+        res.status(401).json({ message: 'Can not send verification email.' });
+    }
+}
+);
+
 const login = asyncHandle(async (req, res) =>
 {
     const { email, password } = req.body;
@@ -84,5 +134,6 @@ const login = asyncHandle(async (req, res) =>
 
 module.exports = {
     register,
-    login
+    login,
+    verification
 };
